cc5d0a476743560bfed9d00966185ed44d23da63
SkyMonder <rilloperdagelo@gmail.com>
2026-04-22 20:20:15 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -e
-
 echo "=== Установка Clover ==="
 mkdir -p temp
 cd temp
-wget -q https://github.com/lucametehau/CloverEngine/releases/download/3.0.3/clover_3.0.3_linux.zip
-python3 -c "import zipfile; zipfile.ZipFile('clover_3.0.3_linux.zip').extractall()" || true
-if [ -f "clover_3.0.3_linux/clover" ]; then
-    cp clover_3.0.3_linux/clover ../engine
-    chmod +x ../engine
-else
-    echo "Clover не найден, пропускаем"
-fi
+wget -q https://github.com/lucametehau/CloverEngine/releases/download/v4.1/clover-4.1-linux.zip
+unzip -q clover-4.1-linux.zip
+cp clover-4.1-linux/clover ../clover_engine
 cd ..
+chmod +x ./clover_engine
 rm -rf temp
-
-echo "=== Запускаем движок Clover ==="
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT engine:app
