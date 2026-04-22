79de90ed304ccb99c237e89e34361714e8b70850
SkyMonder <rilloperdagelo@gmail.com>
2026-04-22 21:23:20 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
-echo "=== Установка Clover ==="
+echo "=== Установка Clover 9.1 ==="
 mkdir -p temp
 cd temp
-wget -q https://github.com/lucametehau/CloverEngine/releases/download/v4.1/clover-4.1-linux.zip
-unzip -q clover-4.1-linux.zip
-cp clover-4.1-linux/clover ../clover_engine
+# Ссылка на последнюю версию
+wget -q https://github.com/lucametehau/CloverEngine/releases/download/9.1/clover-9.1-linux.zip
+unzip -q clover-9.1-linux.zip
+cp clover-9.1-linux/clover ../clover_engine
 cd ..
-chmod +x ./clover_engine
 rm -rf temp
+chmod +x ./clover_engine
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT engine:app
