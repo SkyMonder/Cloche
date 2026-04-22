f36787bb33d475d0f1c4406f4a3b98181699d05e
SkyMonder <rilloperdagelo@gmail.com>
2026-04-22 22:09:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "=== Установка движка из репозитория Stockfish ==="
+echo "=== Установка Reckless 0.9.0 ==="
 mkdir -p temp
 cd temp
-wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
-tar -xf stockfish-ubuntu-x86-64-bmi2.tar
-cp stockfish/stockfish-ubuntu-x86-64-bmi2 ../engine
+
+# Скачивание и распаковка (версия для AVX2)
+wget -q https://github.com/codedeliveryservice/Reckless/releases/download/v0.9.0/Reckless-v0.9.0-avx2.zip
+unzip -q Reckless-v0.9.0-avx2.zip
+
+# Перемещение бинарника в корневую директорию
+cp Reckless-v0.9.0-avx2/reckless-avx2 ../engine
 cd ..
 rm -rf temp
 chmod +x ./engine
